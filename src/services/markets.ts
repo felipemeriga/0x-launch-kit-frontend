7dba3f8d353cc0b962983d03d666f6aeda09e2b5
superdev1994 <sanchzxel@gmail.com>
2020-05-24 10:47:39 -0300

    changed coinmarketcap to coingecko

--- src/services/markets.ts
+++ src/services/markets.ts
@@ -1,19 +1,13 @@
 import { BigNumber } from '@0x/utils';
 
-const ETH_MARKET_PRICE_API_ENDPOINT = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=1027';
+const ETH_MARKET_PRICE_API_ENDPOINT = 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd';
 
 export const getMarketPriceEther = async (): Promise<BigNumber> => {
-    const promisePriceEtherResolved = await fetch(ETH_MARKET_PRICE_API_ENDPOINT, {
-        method: 'GET',
-        headers: {
-            'X-CMC_PRO_API_KEY': '079caa44-3ab2-41ad-86c1-2d7ac2f30325',
-        },
-    });
+    const promisePriceEtherResolved = await fetch(ETH_MARKET_PRICE_API_ENDPOINT);
     if (promisePriceEtherResolved.status === 200) {
         const data = await promisePriceEtherResolved.json();
-        if (data && data[1027]) {
-            const item = data[1027];
-            const priceTokenUSD = new BigNumber(item.quote.USD.price);
+        if (data && data.ethereum) {
+            const priceTokenUSD = new BigNumber(data.ethereum.usd);
             return priceTokenUSD;
         }
     }
