e34db27109d05acd1cf0bf683b5a90e0e7ce811b
Reactor <sanchzxel@gmail.com>
2020-07-03 08:26:49 -0700

    added web3modal

--- src/services/web3_wrapper.ts
+++ src/services/web3_wrapper.ts
@@ -1,13 +1,19 @@
 import { Web3Wrapper } from '@0x/web3-wrapper';
-import Torus from "@toruslabs/torus-embed";
-import Web3 from "web3";
+import Torus from '@toruslabs/torus-embed';
+import Authereum from 'authereum';
+import UniLogin from '@unilogin/provider';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import Web3Modal from 'web3modal';
+import Web3 from 'web3';
 
 import { sleep } from '../util/sleep';
 
 let web3Wrapper: Web3Wrapper | null = null;
+let web3Modal: any = null;
+let web3: any = null;
 
 export const isMetamaskInstalled = (): boolean => {
-    return true;
+    return false;
 };
 
 export const initializeWeb3Wrapper = async (): Promise<Web3Wrapper | null> => {
@@ -16,45 +22,36 @@ export const initializeWeb3Wrapper = async (): Promise<Web3Wrapper | null> => {
     }
 
     const reload = () => {
-        console.log('happened')
-        // window.location.reload();
-    }
+        console.log('happened');
+    };
 
-    if (localStorage.getItem("tokenizer-web3wrapper") === "true") {
-        const torus = new Torus({});
+    const providerOptions = {
+        torus: {
+            package: Torus, // required
+        },
+        authereum: {
+            package: Authereum,
+        },
+        unilogin: {
+            package: UniLogin,
+        },
+        walletconnect: {
+            package: WalletConnectProvider, // required
+            options: {
+                infuraId: '27e484dcd9e3efcfd25a83a78777cdf1', // required
+            },
+        },
+    };
 
-        await torus.init({});
-        const web3 = new Web3(torus.provider as any);
-        web3Wrapper = new Web3Wrapper(web3.currentProvider as any);
-
-        (torus.provider as any).on('chainChanged', reload);
-        (torus.provider as any).on('accountsChanged', reload);
-
-        (torus.provider as any).on('disconnect', () => {
-            localStorage.removeItem("tokenizer-web3wrapper");
-            (torus.provider as any).removeListener('accountsChanged', reload);
-            (torus.provider as any).removeListener('chainChanged', reload);
-            reload();
-        });
-
-        return web3Wrapper;
-    }
-
-    const torus = new Torus({});
-    await torus.init({});
-    await torus.login({});
-    const web3 = new Web3(torus.provider as any);
-    web3Wrapper = new Web3Wrapper(web3.currentProvider as any);
-    localStorage.setItem("tokenizer-web3wrapper", "true");
-
-    (torus.provider as any).on('chainChanged', reload);
-    (torus.provider as any).on('accountsChanged', reload);
-    (torus.provider as any).on('disconnect', () => {
-        localStorage.removeItem("tokenizer-web3wrapper");
-        (torus.provider as any).removeListener('accountsChanged', reload);
-        (torus.provider as any).removeListener('chainChanged', reload);
-        reload();
+    web3Modal = new Web3Modal({
+        providerOptions, // required
+        theme: 'dark',
     });
+
+    const provider = await web3Modal.connect();
+
+    web3 = new Web3(provider);
+    web3Wrapper = new Web3Wrapper(web3.currentProvider as any);
 
     return web3Wrapper;
 };
@@ -66,4 +63,22 @@ export const getWeb3Wrapper = async (): Promise<Web3Wrapper> => {
     }
 
     return web3Wrapper;
+};
+
+export const getWeb3 = async (): Promise<any> => {
+    while (!web3) {
+        // if web3Wrapper is not set yet, wait and retry
+        await sleep(100);
+    }
+
+    return web3;
+};
+
+export const getWeb3Modal = async (): Promise<any> => {
+    while (!web3Modal) {
+        // if web3Wrapper is not set yet, wait and retry
+        await sleep(100);
+    }
+
+    return web3Modal;
 };
